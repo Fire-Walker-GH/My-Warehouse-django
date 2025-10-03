23a72231692301fb40c23bf190225940c2025cb1
Vldr <iskateligr@gmail.com>
2025-10-03 17:18:28 +0300

    Добавил главную страницу сайта, модели Warehouse и Item

---
+++ main/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Warehouse (models.Model):
+    name = models.CharField(max_length = 100)
+    address = models.CharField(max_length = 150)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='warehouses' )
+
+    def __str__(self):
+        return self.name
+
+
+class Item (models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    quantity = models.PositiveIntegerField(default=0)
+    Warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE, related_name='items')
+
+    def __str__(self):
+        return self.name
+    
