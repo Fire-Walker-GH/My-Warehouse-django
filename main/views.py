b029de09ec5d0356beb257b578fecd7542ec37a5
Vldr <iskateligr@gmail.com>
2025-10-23 16:33:44 +0300

    Страница авторизации, кнопка выхода из аккаунта, изменена структура css файлов, главная страница меняется для авторизованных пользователей

--- main/views.py
+++ main/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 
 def home(request):
     return render(request, 'main/home.html')
 
-def register(request):
+def user_register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -16,3 +16,21 @@ def register(request):
     else:
         form = UserCreationForm()  # Пустая форма для GET-запроса
     return render(request, 'main/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'main/login.html', {'form': form})
+
+def user_logout(request):
+     logout(request)
+     return redirect('home')
