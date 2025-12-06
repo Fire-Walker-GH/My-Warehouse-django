7dde466ff209d619c1f8a608f18df7a754c45a1f
Vldr <iskateligr@gmail.com>
2025-12-06 21:43:26 +0300

    Обновление безопасности

--- MyWarehouse/settings.py
+++ MyWarehouse/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--mjj+f%fy+n1%1jnt8-s6zm54harldhkkh$pl%6u#mjoa!e7s$'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
